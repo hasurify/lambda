4d91a9cf7cb7c8ef111fbfaff7edab00b2334065
Ken Le <ken@kenstack.io>
2023-08-28 11:11:29 +0700

    fix(logger): remove color CLI package

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,3 @@
-import { red, yellow, blue, green } from 'cli-color';
-
 const LEVEL_INFO = 'INFO';
 const LEVEL_WARN = 'WARN';
 const LEVEL_ERROR = 'ERROR';
@@ -12,33 +10,21 @@ const KEY_MESSAGE = 'message';
 export class AppLogger {
   error(...logs: any) {
     const logString = this.#buildLog(LEVEL_ERROR, logs);
-    if (process.env.ENVIRONMENT === 'local') {
-      return console.error(red(logString));
-    }
     return console.error(logString);
   }
 
   info(...logs: any) {
     const logString = this.#buildLog(LEVEL_INFO, logs);
-    if (process.env.ENVIRONMENT === 'local') {
-      return console.info(blue(logString));
-    }
     return console.info(logString);
   }
 
   warn(...logs: any) {
     const logString = this.#buildLog(LEVEL_WARN, logs);
-    if (process.env.ENVIRONMENT === 'local') {
-      return console.warn(yellow(logString));
-    }
     return console.warn(logString);
   }
 
   success(...logs: any) {
     const logString = this.#buildLog(LEVEL_DEBUG, logs);
-    if (process.env.ENVIRONMENT === 'local') {
-      return console.log(green(logString));
-    }
     return console.log(logString);
   }
 
